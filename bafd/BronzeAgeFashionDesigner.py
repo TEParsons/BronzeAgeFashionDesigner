a27a4674a49a760cad83e9b89604ca9e0ad1a86b
Todd <todd.e.parsons@gmail.com>
2021-04-18 20:11:17 +0100

    Update main script to test designer

--- bafd/BronzeAgeFashionDesigner.py
+++ bafd/BronzeAgeFashionDesigner.py
@@ -1,15 +1,11 @@
 import pygame
-from packaging.version import Version
 
-from bafd import sprites, utils
-from bafd.scenes import map
-
-# Mark version number
-__version__ = Version("0.0.0")
+from bafd import sprites, utils, scenes
 
 # Setup basic requirements
 pygame.init()
 clock = pygame.time.Clock()
+
 font = pygame.font.SysFont("monospace", 6)
 # Setup window and virtual window
 win = pygame.display.set_mode(utils.window.wsize)
@@ -17,20 +13,21 @@ vwin = utils.window.VirtualWindow(utils.window.vsize)
 # Style window
 pygame.display.set_caption('Bronze Age Fashion Designer')
 pygame.display.set_icon(sprites.logo)
+# Create map view
+main_map = scenes.map.Map(utils.window.vsize)
+# Create designer view
+designer_view = scenes.designer.Designer(utils.window.vsize)
 
-main_map = map.Map(vwin)
+vwin.scene = designer_view
 
-
-pygame.time.wait(2000)
-# # Pass 10 years
-# year = 0
-# while year <= 10:
-#     pygame.time.wait(1000)
-#     map.appeal.player['greek'] += 20
-#     map.appeal.calculate_appeal(map.appeal.player)
-#     for culture in map.influence.influence:
-#         map.influence.influence[culture] += map.appeal.appeal["player"][culture]
-#     main_map.update_overlay()
-#     year += 1
-
-
+done = False
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            done = event.key == pygame.K_ESCAPE
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if hasattr(vwin.scene, "on_click"):
+                pos = utils.window.Position(*pygame.mouse.get_pos())
+                vwin.scene.on_click(pos)
+    clock.tick(60)
+    pygame.display.flip()
